8953d0f33c86bb5234b24e987f5ebe32ef880919
Scattercatt <91511911+Scattercatt@users.noreply.github.com>
2023-05-18 18:42:10 -0700

    More foundational code. Specifically the C

--- cpu_render/render_column.c
+++ cpu_render/render_column.c
@@ -3,30 +3,24 @@
 #include "..\fractal_definitions.c"
 #include <complex.h>
 
-struct Point {
-    int x;
-    int y;
-};
 
 int main(int argc, char *argv[])
 {   
-    if (argc < 7)
+    const int EXPECTED_ARG_COUNT = 8;
+
+    if (argc < EXPECTED_ARG_COUNT)
     {
-        printf("Must specify all args");
+        printf("Must specify all args for render_column.c");
         return 1;
     }
 
     // Arg 1 is X position of point 1
     // Arg 2 is Y position of point 1
-    struct Point p1;
-    p1.x = atoi(argv[1]);
-    p1.x = atoi(argv[2]);
+    complex double p1 = strtod(argv[1], NULL) + strtod(argv[2], NULL) * I;
 
     // Arg 3 is X position of point 2
     // Arg 4 is Y position of point 2
-    struct Point p2;
-    p2.x = atoi(argv[3]);
-    p2.x = atoi(argv[4]);
+    complex double p2 = strtod(argv[3], NULL) + strtod(argv[4], NULL) * I;
 
     // Arg 5 is the size of the desired output array
     int img_size = atoi(argv[5]);
@@ -34,10 +28,23 @@ int main(int argc, char *argv[])
     // Arg 6 is the column number
     int column_to_render = atoi(argv[6]);
 
-    complex double test1 = 0.1 + 1.2 * I;
-    complex double test2 = 0.1 + 1.2 * I;
+    // Arg 7 is the max iteration count
+    int max_iterations = atoi(argv[7])
 
-    complex double test = mandelbrot_set(test1, test2);
+    if (column_to_render > img_size)
+    {
+        printf("Render column cannot be greater than image size!");
+        return 1;
+    }
 
-    printf("%lf %lf", creal(test), cimag(test));
+    // Column separation amount
+    double column_separation_amount = (creal(p2) - creal(p1)) / (float) img_size;
+
+    // Column double to render across
+    double column_to_render_across = column_separation_amount * column_to_render;
+
+    for (double i = cimag(p1); i < cimag(p2); i += column_separation_amount)
+    {
+
+    }
 }
